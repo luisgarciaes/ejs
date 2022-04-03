7be9e4ac467c5b826fa638dd9973d8f4a6e596b1
ruruisu <zaddozgl@gmail.com>
2022-04-03 18:24:22 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+//sets the view engine to ejs
+app.set("view engine", "ejs");
+app.use("/assets", express.static(__dirname + "/public"));
+app.use("/", (req, res, next) => {
+  console.log("Request URL:" + req.url);
+  next();
+});
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/api", (req, res) => {
+  res.json({ firstname: "Luis", lastname: "Garcia" });
+});
+app.get("/number/:id", (req, res) => {
+  res.render("number", { ID: req.params.id });
+});
+app.listen(port);
